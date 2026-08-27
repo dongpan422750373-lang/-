c45a0a0e2fce3139a2ec6e4b458855994cae279c
dongpan422750373-lang <dongpan422750373@gmail.com>
2026-08-28 07:09:10 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+// 改动 index.html 后，把版本号 +1，手机上刷新一次就会更新
+const V = 'beishu-v1';
+const FILES = ['./', './index.html', './manifest.json',
+               './icon-180.png', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(V).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(ks => Promise.all(ks.filter(k => k !== V).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// 缓存优先：装好之后完全离线可用，顺便后台悄悄更新
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit => {
+      const net = fetch(e.request).then(res => {
+        if (res && res.status === 200 && res.type === 'basic') {
+          const copy = res.clone();
+          caches.open(V).then(c => c.put(e.request, copy));
+        }
+        return res;
+      }).catch(() => hit);
+      return hit || net;
+    })
+  );
+});
