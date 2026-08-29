1cc542af4154831b91ff1bd4c2a24a7f50bb7fc6
dongpan422750373-lang <dongpan422750373@gmail.com>
2026-08-30 01:30:50 +0800

    [iPhone] 发布背书 beishu-v13：sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
 // 改动 index.html 后，把版本号 +1；v10 起会自动发现并切换新版
-const V = 'beishu-v12';
+const V = 'beishu-v13';
 const FILES = ['./', './index.html', './manifest.json',
-               './icon-180.png', './icon-192.png', './icon-512.png',
-               './assets/ranks/bronze.png', './assets/ranks/silver.png',
-               './assets/ranks/gold.png', './assets/ranks/platinum.png',
-               './assets/ranks/diamond.png', './assets/ranks/challenger.png'];
+               './icon-180.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   const freshFiles = FILES.map(f=>new Request(f,{cache:'reload'}));
